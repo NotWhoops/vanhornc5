2ecf94bd13ff7465b9b694697d3975a0faf0719f
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-04-02 16:23:58 -0700

    added toggle button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Getdatabutton from "@/components/Databutton";
+import { myData } from "@/lib/dataFetch";
 
 export default function Home() {
   return (    
     <div>
-      <p>Whereas recognition of the inherent dignity</p>
+      <h1>Social Media Dashboard</h1>
+      <p>Total Followers:</p>
+
+      <div className="text-gray-400">
+        Dark Mode
+        <label htmlFor="check" className="flex bg-[#378fe6] bg-linear-to-r from-[#378fe6] to-[#3eda82] cursor-pointer relative w-20 h-10 rounded-full">
+          <input type="checkbox" id="check" className="sr-only peer"/>
+          <span className="w-2/5 h-4/5 bg-black absolute rounded-full left-1 top-1 peer-checked:bg-white peer-checked:left-11 transition-all duration-500" />
+        </label>
+      </div>
     </div>
   );
 }
--- components/Databutton.tsx
+++ components/Databutton.tsx
@@ -1,21 +1,121 @@
 'use client'
 
-import { myData } from '@/lib/dataFetch'
+import { myData, myData2, myData3, myData4, myData5, myData6 } from '@/lib/dataFetch'
 import React, { useState } from 'react'
 
 const Getdatabutton = () => {
     console.log("component is rendering");
 
     const [data, setData] = useState("Here is your text");
+    const [data2, setData2] = useState("Here is your text");
+    const [data3, setData3] = useState("Here is your text");
+    const [data4, setData4] = useState("Here is your text");
+    const [data5, setData5] = useState("Here is your text");
+    const [data6, setData6] = useState("Here is your text");
 
     const handleClick = async () => {
         console.log("Function is running");
         setData(await myData());
     }
+    const handleClick2 = async () => {
+        console.log("Function is running");
+        setData(await myData2());
+    }
+    const handleClick3 = async () => {
+        console.log("Function is running");
+        setData(await myData3());
+    }
+    const handleClick4 = async () => {
+        console.log("Function is running");
+        setData(await myData3());
+    }
+    const handleClick5 = async () => {
+        console.log("Function is running");
+        setData(await myData4());
+    }
+    const handleClick6 = async () => {
+        console.log("Function is running");
+        setData(await myData5());
+    }
+    const handleClick7 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick8 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick9 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick10 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick11 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick12 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick13 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick14 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick15 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick16 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick17 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick18 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick19 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick20 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick21 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick22 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick23 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick24 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
+    const handleClick25 = async () => {
+        console.log("Function is running");
+        setData(await myData6());
+    }
 
   return (
     <div>
-        <button onClick={handleClick}>Click</button>
         <p>{data}</p>
     </div>
   )
